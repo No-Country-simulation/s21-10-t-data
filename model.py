85095f3bf9cafeee883494bdebe7eda577cd7644
Isleg <mikeismerio@gmail.com>
2025-03-03 19:01:09 -0600

    Update model.py

--- model.py
+++ model.py
@@ -2,30 +2,9 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
 from PIL import Image
 import os
-
-# Definir el modelo simplificado
-def create_model():
-    model = Sequential([
-        Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Flatten(),
-        Dense(256, activation='relu'),
-        Dropout(0.3),
-        Dense(4, activation='softmax')
-    ])
-    return model
 
 # Clases
 CLASS_TYPES = ['Pituitary', 'No Tumor', 'Meningioma', 'Glioma']
@@ -40,8 +19,7 @@ if uploaded_model is not None:
     with open("temp_model.h5", "wb") as f:
         f.write(uploaded_model.getbuffer())
     
-    model = create_model()
-    model.load_weights("temp_model.h5")
+    model = load_model("temp_model.h5")  # Cargar modelo completo en vez de solo pesos
     st.success("Modelo cargado exitosamente")
 
     uploaded_file = st.file_uploader("Sube una imagen de MRI", type=["jpg", "png", "jpeg"])
