f305943e799c889334109647fb1607b6da6a7305
Isleg <mikeismerio@gmail.com>
2025-03-03 19:21:52 -0600

    Update model.py

--- model.py
+++ model.py
@@ -8,22 +8,26 @@ from PIL import Image
 import os
 import h5py
 
-# Definir la arquitectura del modelo en caso de que solo se carguen los pesos
+# Definir la arquitectura del modelo original
+# Esta es la misma arquitectura utilizada para entrenar los pesos
+
 def create_model():
     model = Sequential([
-        Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
-        
+        Conv2D(32, (4, 4), activation="relu", input_shape=(150, 150, 3)),
+        MaxPooling2D(pool_size=(3, 3)),
+
+        Conv2D(64, (4, 4), activation="relu"),
+        MaxPooling2D(pool_size=(3, 3)),
+
+        Conv2D(128, (4, 4), activation="relu"),
+        MaxPooling2D(pool_size=(3, 3)),
+
+        Conv2D(128, (4, 4), activation="relu"),
         Flatten(),
-        Dense(256, activation='relu'),
-        Dropout(0.3),
-        Dense(4, activation='softmax')
+
+        Dense(512, activation="relu"),
+        Dropout(0.5),
+        Dense(4, activation="softmax")
     ])
     return model
 
@@ -43,7 +47,7 @@ if uploaded_model is not None:
     # Verificar si el archivo contiene un modelo completo o solo pesos
     with h5py.File("temp_model.h5", "r") as file:
         if "model_weights" in file.keys():
-            st.warning("El archivo solo contiene pesos, se usará un modelo predefinido.")
+            st.warning("El archivo solo contiene pesos, se usará la arquitectura original del modelo.")
             model = create_model()
             model.load_weights("temp_model.h5")
         else:
