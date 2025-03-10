32740ac7efd85549180b61b19939238f065c7c7c
Isleg <mikeismerio@gmail.com>
2025-03-10 16:59:39 -0600

    Update detection2.py

--- detection2.py
+++ detection2.py
@@ -17,7 +17,7 @@ st.write(f"🔹 **Versión de TensorFlow:** `{tf.__version__}`")
 
 # =================== CARGAR MODELO ===================
 MODEL_H5_PATH = "braintumor2.h5"
-MODEL_TF_PATH = "braintumor2"
+MODEL_TF_PATH = "braintumor2.h5"
 
 st.write(f"📥 **Cargando modelo...**")
 
